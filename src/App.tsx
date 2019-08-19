4f30c41b8907305480db6751ec66f58aa2db381d
Anish Kargaonkar <anishkargaonkar@gmail.com>
2019-08-19 08:22:14 +0530

    add redux & redux-saga

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,41 @@
 import React from 'react';
-import logo from './logo.svg';
+import { createBrowserHistory } from 'history';
+import { Provider } from 'react-redux';
+import { ConnectedRouter } from 'connected-react-router';
+import { ApplicationState } from './store';
+import configureStore from './configureStore';
 import './App.css';
+import logo from './logo.svg';
+
+const history = createBrowserHistory();
+
+const initialState: ApplicationState = {
+  router: ({ location: history.location, action: 'PUSH' })
+}
+
+const store = configureStore(history, initialState)
 
 const App: React.FC = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
+    <Provider store={store}>
+      <ConnectedRouter history={history}>
+        <div className="App">
+          <header className="App-header">
+            <img src={logo} className="App-logo" alt="logo" />
+            <p>Edit <code>src/App.tsx</code> and save to reload.</p>
+            <a
+              className="App-link"
+              href="https://reactjs.org"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              Learn React
         </a>
-      </header>
-    </div>
+          </header>
+        </div>
+
+      </ConnectedRouter>
+    </Provider>
   );
 }
 
